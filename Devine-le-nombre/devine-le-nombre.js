e952e9796df0c63e3e6c2254be087e7d9fb4865d
Estelle_alz <136736604+estellealz@users.noreply.github.com>
2024-01-26 09:02:35 +0100

    Update devine-le-nombre.js
    Commentaires

--- Devine-le-nombre/devine-le-nombre.js
+++ Devine-le-nombre/devine-le-nombre.js
@@ -1,10 +1,10 @@
 /*https://www.linkedin.com/in/estelle-alizier-5b1208298/*/
 // JEU NOMBRE 
 
-// 1 Générer un nombre aléatoire
+// Etape 1: Générer un nombre aléatoire
 let randomNumber;
 
-// 2 Sélectionner les éléments HTML pour manipulation ultérieure
+// Etape 2 : Sélectionner les éléments HTML pour manipulation ultérieure
 let guessField = document.getElementById('guessField');
 let guessSubmit = document.getElementById('guessSubmit');
 let reset = document.getElementById('reset');
@@ -12,16 +12,19 @@ let guesses = document.getElementById('guesses');
 let lastResult = document.getElementById('lastResult');
 let lowOrHigh = document.getElementById('lowOrHigh');
 
+// Etape 3 : Initialisation du compteur
 let guessCount = 0; // Ajout du compteur d'essais
 
+// Etape 4 : Générer un nombre aléatoire de 0 à 100
 function generateRandomNumber() {
     return Math.floor(Math.random() * 100);
 }
 
+// Etape 5 : Fonction pour commencer le jeu
 function startGame() {
     randomNumber = generateRandomNumber();
-    console.log(randomNumber) //voir le bon nombre
-    guessCount = 0;
+    console.log(randomNumber) // Voir le bon nombre sur la console
+    guessCount = 0; // Initialisation à 0 du nombre de tentatives
     guesses.textContent = guessCount;
     lastResult.textContent = '';
     lowOrHigh.textContent = '';
@@ -29,12 +32,14 @@ function startGame() {
     guessSubmit.disabled = false;
 }
 
+// Etape 6 : Lancer le jeu
 startGame();
 
 guessSubmit.addEventListener("click", function () {
     let guess = parseInt(guessField.value);
     console.log("Tentative : " + guess); //afficher sur la console les anciens numéros
 
+    // Condition moins de 10 tentatives
     if (guessCount < 10) {
         guessCount++;
 
